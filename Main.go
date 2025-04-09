d3489d37d4cd18a1700211075d227f9b87e08e5b
willpiam <willdoyle422@gmail.com>
2025-04-09 13:31:38 -0400

    make go go faster

--- Main.go
+++ Main.go
@@ -8,58 +8,47 @@ import (
 )
 
 func ruleToBinaryArray(ruleNumber int) []int {
-    binary := fmt.Sprintf("%08b", ruleNumber)
     ruleBinary := make([]int, 8)
-
-    for i, ch := range binary {
-        if ch == '1' {
-            ruleBinary[7-i] = 1
-        } else {
-            ruleBinary[7-i] = 0
-        }
+    for i := 0; i < 8; i++ {
+        ruleBinary[i] = (ruleNumber >> i) & 1
     }
-
     return ruleBinary
 }
 
-func calculateCell(neighborhood string, ruleBinary []int) int {
-    index, _ := strconv.ParseInt(neighborhood, 2, 64)
-    return ruleBinary[index]
+func calculateCell(neighborhood int, ruleBinary []int) int {
+    return ruleBinary[neighborhood]
 }
 
 func runCellularAutomaton(ruleNumber, generations int, initialConditions string) [][]int {
     cells := make([]int, len(initialConditions))
     for i, bit := range initialConditions {
-        if bit == '1' {
-            cells[i] = 1
-        } else {
-            cells[i] = 0
-        }
+        cells[i] = int(bit - '0')
     }
 
     ruleBinary := ruleToBinaryArray(ruleNumber)
     imageWidth := len(cells) + 2*generations
-    var automatonData [][]int
+    automatonData := make([][]int, generations)
+    nextGeneration := make([]int, imageWidth)
 
     for i := 0; i < generations; i++ {
         paddingLength := (imageWidth - len(cells)) / 2
-        padding := make([]int, paddingLength)
-        extendedCells := append(append(padding, cells...), padding...)
+        extendedCells := make([]int, imageWidth)
+        copy(extendedCells[paddingLength:], cells)
 
-        automatonData = append(automatonData, extendedCells)
+        automatonData[i] = make([]int, imageWidth)
+        copy(automatonData[i], extendedCells)
 
-        nextGeneration := make([]int, len(extendedCells))
-        for j := range extendedCells {
+        for j := 0; j < imageWidth; j++ {
             leftNeighbor := 0
             if j > 0 {
                 leftNeighbor = extendedCells[j-1]
             }
             rightNeighbor := 0
-            if j < len(extendedCells)-1 {
+            if j < imageWidth-1 {
                 rightNeighbor = extendedCells[j+1]
             }
 
-            neighborhood := fmt.Sprintf("%d%d%d", leftNeighbor, extendedCells[j], rightNeighbor)
+            neighborhood := (leftNeighbor << 2) | (extendedCells[j] << 1) | rightNeighbor
             nextGeneration[j] = calculateCell(neighborhood, ruleBinary)
         }
         cells = nextGeneration
@@ -82,16 +71,22 @@ func outputToFile(automatonData [][]int, ruleNumber, generations int, initialCon
         imageWidth = len(automatonData[0])
     }
 
-    fmt.Fprintf(writer, "P1\n%d %d\n", imageWidth, generations)
+    // Pre-allocate buffer for the header
+    header := fmt.Sprintf("P1\n%d %d\n", imageWidth, generations)
+    writer.WriteString(header)
+
+    // Pre-allocate buffer for each row
+    rowBuffer := make([]byte, imageWidth+1) // +1 for newline
     for _, row := range automatonData {
-        for _, cell := range row {
+        for j, cell := range row {
             if cell == 1 {
-                writer.WriteString("1")
+                rowBuffer[j] = '1'
             } else {
-                writer.WriteString("0")
+                rowBuffer[j] = '0'
             }
         }
-        writer.WriteString("\n")
+        rowBuffer[imageWidth] = '\n'
+        writer.Write(rowBuffer)
     }
     writer.Flush()
 }
