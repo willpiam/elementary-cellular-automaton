ac9e74730484ff5882233a40ac68141f623ecfbe
willpiam <willdoyle422@gmail.com>
2025-04-29 12:05:08 -0400

    make scala faster

--- Main.scala
+++ Main.scala
@@ -1,50 +1,43 @@
 import scala.io.Source
 import java.io.PrintWriter
-import scala.collection.mutable.ListBuffer
 
 object CellularAutomaton: 
 
-  def ruleToBinaryArray(ruleNumber: Int): Array[Int] = 
-    val binaryString = ruleNumber.toBinaryString.reverse.padTo(8, '0').reverse
-    binaryString.map(_.asDigit).toArray
+  def ruleToBinaryArray(ruleNumber: Int): Array[Int] =
+    Array.tabulate(8)(i => (ruleNumber >> i) & 1)
 
-  def calculateCell(pState: String, rule: Array[Int]): Int = 
-    val ruleMap = Map(
-      "111" -> rule(0),
-      "110" -> rule(1),
-      "101" -> rule(2),
-      "100" -> rule(3),
-      "011" -> rule(4),
-      "010" -> rule(5),
-      "001" -> rule(6),
-      "000" -> rule(7)
-    )
-    ruleMap(pState)
-
-  def runCellularAutomaton(ruleNumber: Int, generations: Int, initialConditions: String): Unit = 
-    var cells = initialConditions.map(_.asDigit).toArray
-
+  def runCellularAutomaton(ruleNumber: Int, generations: Int, initialConditions: String): Unit =
+    val initArr = initialConditions.view.map(_.asDigit).toArray
+    val imageWidth = initArr.length + 2 * generations
     val ruleBinary = ruleToBinaryArray(ruleNumber)
 
-    val imageWidth = cells.length + 2 * generations
-    val imageData = new StringBuilder(s"P1\n$imageWidth $generations\n")
+    new java.io.File("results").mkdirs()
+    val writer = new PrintWriter(s"results/r${ruleNumber}_g${generations}_i${initialConditions}_scala.pbm")
+    writer.println(s"P1\n$imageWidth $generations")
 
-    for (_ <- 0 until generations) 
-      val paddingLength = (imageWidth - cells.length) / 2
-      val padding = Array.fill(paddingLength)(0)
-      val extendedCells = padding ++ cells ++ padding
+    val current = Array.fill(imageWidth)(0)
+    val next    = Array.ofDim[Int](imageWidth)
+    Array.copy(initArr, 0, current, generations, initArr.length)
+    val rowChars = new Array[Char](imageWidth + 1)
 
-      imageData.append(extendedCells.map(cell => if (cell == 0) '0' else '1').mkString + "\n")
+    for (_ <- 0 until generations) do
+      var j = 0
+      while j < imageWidth do
+        rowChars(j) = if current(j) == 1 then '1' else '0'
+        j += 1
+      rowChars(imageWidth) = '\n'
+      writer.write(rowChars)
 
-      val nextGeneration = new ListBuffer[Int]()
-      for (j <- 1 until extendedCells.length - 1) 
-        val neighborhood = s"${extendedCells(j - 1)}${extendedCells(j)}${extendedCells(j + 1)}"
-        nextGeneration += calculateCell(neighborhood, ruleBinary)
-      cells = nextGeneration.toArray
+      var i = 0
+      while i < imageWidth do
+        val l = if i > 0                 then current(i-1) else 0
+        val c =                             current(i)
+        val r = if i < imageWidth - 1 then current(i+1) else 0
+        next(i) = ruleBinary((l << 2) | (c << 1) | r)
+        i += 1
+      System.arraycopy(next, 0, current, 0, imageWidth)
 
-    new PrintWriter(s"results/r${ruleNumber}_g${generations}_i${initialConditions}_scala.pbm") {
-      write(imageData.toString); close()
-    }
+    writer.close()
 
   def readInputsFromFile(filePath: String): (Int, String, Int) = 
     val lines = Source.fromFile(filePath).getLines().toList
